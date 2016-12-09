10fec027c81e92a001d21e51453fb179438ccbc3
mradambeck <mr.adambeck@gmail.com>
2016-12-09 15:43:55 -0800

    can CRUD notes, need to add color picker

--- js/index.js
+++ js/index.js
@@ -3,44 +3,125 @@ console.log("index.js called");
 $( document ).ready( function(){
   console.log("document ready");
 
-  var noteStorage = {};
+  var noteCount = 0,
+      noteId = 300;
 
-  var noteCount = 0;
-
-  var title = "Test title";
 
   function Note(title, count){
     this.content = "";
     this.title = title;
     this.count = count;
   }
+  var noteStorage = {};
+
+  function setFieldsToDisabled(bool){
+    $('#note-body > textarea').prop("disabled", bool);
+    $('button#delete-note').prop("disabled", bool);
+    if (bool){
+      $('button#delete-note').css('cursor','not-allowed');
+      $('#note-body > textarea').css('cursor','not-allowed');
+    } else {
+      $('button#delete-note').css('cursor','pointer');
+      $('#note-body > textarea').css('cursor','default');
+    }
+  }
+
+  function switchMobileToView(page){
+    if (page === "notepad"){
+      $('#note-selection').addClass('mobile-hide');
+      $('#note-content').removeClass('mobile-hide');
+    } else {
+      $('#note-selection').removeClass('mobile-hide');
+      $('#note-content').addClass('mobile-hide');
+    }
+  }
+
+  setFieldsToDisabled(true);
 
   // Note creation
-  $('#new-note').on('click', function(){
+  $('button#new-note').on('click', function(){
 
-    // Generate a new Note, push to
-    noteStorage['note-' + noteCount] = new Note((title + ": " + noteCount), noteCount);
-    var thisNote = noteStorage['note-' + noteCount];
+    setFieldsToDisabled(false);
 
-    // ERASE ME LATER
-    noteStorage['note-' + noteCount].content = ("Yo duuuuuude " + noteCount);
+    function createNote(){
+      var title = prompt("Please name your note:");
+      if (title === null){
+        return false;
+      }
+
+      // Generate a new Note, push to
+      noteStorage['note-' + noteCount] = new Note(title, noteCount);
+      var thisNote = noteStorage['note-' + noteCount];
+
+      var noteHtml = $("<div class='col-100 note-select' data-note-count=" + thisNote.count + ">" + thisNote.title + "</div>");
+      $('#notes-list').append(noteHtml);
+      noteCount++;
+
+      // Switch views on mobile
+      switchMobileToView("notepad");
 
 
-    var listNote = $("<div class='col-100 note-select' data-note-count=" + thisNote.count + ">" + thisNote.title + "</div>");
-    $('#notes-list').append(listNote);
-    noteCount++;
+      // Update note content
+      $('#note-content').attr("data-current-note", thisNote.count);
+      $('#note-body > textarea').val(thisNote.content);
+      $('#highlighted-note-title > h2').text(thisNote.title);
+    }
 
-    $('#note-selection').addClass('mobile-hide');
-    $('#note-content').removeClass('mobile-hide');
-    $('#note-content').data("note-count", thisNote.count);
-    $('#highlighted-note-title > h2').text(thisNote.title);
+    createNote();
+
   });
 
+
+
+  // Select existing note
   $('#note-selection').on('click', '.note-select', function(){
-    var noteID = $(this).data('note-count');
+    setFieldsToDisabled(false);
+
+    noteID = $(this).data('note-count');
     var thisNote = noteStorage['note-' + noteID];
+
+    // Switch views on mobile
+    $('#note-selection').addClass('mobile-hide');
+    $('#note-content').removeClass('mobile-hide');
+
+    // Update content
+    $('#note-content').attr("data-current-note", thisNote.count);
     $('#highlighted-note-title > h2').text(thisNote.title);
-    $('#note-body > textarea').text(thisNote.content);
+    $('#note-body > textarea').val(thisNote.content);
+
+    switchMobileToView("notepad");
+  });
+
+
+
+  // Update note content upon change
+  $('#note-body > textarea').keyup(function(){
+    var thisNoteId = $('#note-content').attr('data-current-note');
+    var thisNote = noteStorage['note-' + thisNoteId];
+
+    var updatedContent = $(this).val();
+    thisNote.content = updatedContent;
+  });
+
+
+
+  // Delete a note
+  $('button#delete-note').on('click', function(){
+
+    var thisNoteId = $('#note-content').attr('data-current-note');
+    var noteSelector = $('.note-select[data-note-count="' + thisNoteId + '"]');
+    $('#highlighted-note-title > h2').text("");
+    $('#note-body > textarea').val('');
+    $(noteSelector).remove();
+
+    delete noteStorage['note-' + thisNoteId];
+    setFieldsToDisabled(true);
+    switchMobileToView("note-select");
+  });
+
+  // (mobile) return to notes list
+  $('button#back-button').on('click', function(){
+    switchMobileToView("note-select");
   });
 
 });
